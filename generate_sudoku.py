26374cd1ca6dd70a76f0fabec2b2feb767ab4474
DOMOKOON <qlehf0703@gmail.com>
2024-10-28 13:31:34 +0900

    Update generate_sudoku.py

--- generate_sudoku.py
+++ generate_sudoku.py
@@ -1,5 +1,19 @@
-# generate_sudoku.py
+import random
+
 def generate_sudoku():
-    """빈 칸이 포함된 스도쿠 퍼즐을 생성합니다."""
-    # 스도쿠 퍼즐을 생성하는 로직을 구현합니다.
-    pass
+
+    puzzle = []
+    # 0의 확률을 50%으로 고정 - 전체 18개 중 9개가 0이므로!
+    numbers = [0] * 9 + list(range(1, 10))
+    for _ in range(9):
+        row = []
+        for _ in range(9):
+            #rand_int = random.randint(0, 9)
+            rand_int = random.choice(numbers)
+            row.append(rand_int)
+        puzzle.append(row)
+
+    return puzzle
+
+sudoku_puzzle = generate_sudoku()
+print_board(sudoku_puzzle)
